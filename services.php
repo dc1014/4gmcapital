96b5cb5f2f0bfd54e6c9b18cea9a6abd3301cc7a
Daniel Casper <Daniel Casper>
2017-01-03 19:18:20 -0600

    Add strong list headers and style with vc_li.

--- services.php
+++ services.php
@@ -39,34 +39,34 @@
             <h2><span class="vc_main-color">Services</span></h2>
             <p>4GM Capital is at your disposal to engage in any manner of Business Growth Initiative that you may be contemplating. Our strengths in analysis, investigation, discovery, management, planning, implementation, integration, and optimization can be applied to many aspects of your business. Typical projects engagements may consist of one or all of the following in any combination:</p>
 
-            <ul>
-            	<li>Business Process Evaluation
+            <ul class="vc_li">
+            	<li><strong>Business Process Evaluation</strong>
             		<ul class="vc_li">
             			<li>Are your New Product Introduction, Sales/Inventory Planning, Order Entry and Fulfillment processes etc., operating well or in need of some TLC?
             		</ul>
             	</li>
-            	<li>Cross-functional Process Implementation
+            	<li><strong>Cross-functional Process Implementation</strong>
             		<ul class="vc_li"><li>Are your functional area silos of expertise? Let us link your organizations for maximum effectiveness.</li></ul>
             	</li>
-            	<li>Product and Technology Roadmap Review
+            	<li><strong>Product and Technology Roadmap Review</strong>
             		<ul class="vc_li"><li>Do you really produce things that customers want to buy? Let us objectively review.</li></ul>
             	</li>
-            	<li>Go-to-Market Review Strategy
+            	<li><strong>Go-to-Market Review Strategy</strong>
             		<ul class="vc_li"><li>Are you reaching the right audience and are your promotional spends effective?</li></ul>
             	</li>
-            	<li>Business Strategy Review
+            	<li><strong>Business Strategy Review</strong>
             		<ul class="vc_li"><li>What are you really good at and what should you not be doing? The answer may surprise you.</li></ul>
             	</li>
-            	<li>Deep Market Niche Study
+            	<li><strong>Deep Market Niche Study</strong>
             		<ul class="vc_li"><li>We can drill down and uncover information that typical research can't or won't.</li></ul>
             	</li>
-            	<li>Functional Organization Evaluation
+            	<li><strong>Functional Organization Evaluation</strong>
             		<ul class="vc_li"><li>Processes and management are important, but so is having the right expertise at the right cost.</li></ul>
             	</li>
-            	<li>Deep Due Diligence
+            	<li><strong>Deep Due Diligence</strong>
             		<ul class="vc_li"><li>Are you really asking the right questions of the right people? Let us dive deep.</li></ul>
             	</li>
-            	<li>Acquisition Integration
+            	<li><strong>Acquisition Integration</strong>
             		<ul class="vc_li"><li>We can make the task of integration acquisitions much less daunting.</li></ul>
             	</li>
 			</ul><br>
